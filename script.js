0b3267446b4078468a4db8652f394c24e2c64cbd
noexcuz99-cmd <noexcuz99@gmail.com>
2026-04-19 08:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function openApp(url) {
   window.location.href = url;
 }
+
+/* Banner Slider */
+const images = [
+  "https://via.placeholder.com/350x150/ff7f7f",
+  "https://via.placeholder.com/350x150/7fbfff",
+  "https://via.placeholder.com/350x150/7fff7f"
+];
+
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  document.getElementById("slider").src = images[index];
+}, 2000);
